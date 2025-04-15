1e6f879c69362a45bf9e22394d371ef5aafb29ef
YamanoYuma <mrp.yamano@gmail.com>
2025-04-15 12:52:57 +0900

    memoApp完成

--- vanillaJs-memoApp/index.js
+++ vanillaJs-memoApp/index.js
@@ -3,8 +3,7 @@
 const onClickAdd = () => {
   const textEl = document.getElementById("add-text");
   const text = textEl.value;
-  
-  // inputを空に
+
   textEl.value = "";
 
   const li = document.createElement("li");
@@ -13,20 +12,19 @@ const onClickAdd = () => {
   p.textContent = text;
 
 
-  const button = document.getElementById("button");
+  const button = document.createElement("button");
   button.textContent = "削除";
 
   button.addEventListener("click", () => {
     const deleteTarget = button.closest("li");
-    
-  })
-  //デバック用
-  console.log(text);
+    document.getElementById("memo-list").removeChild(deleteTarget);
+  });
+  div.appendChild(p);
+  div.appendChild(button);
+  li.appendChild(div);
+
+  document.getElementById("memo-list").appendChild(li);
 }
 document.getElementById("add-button").addEventListener('click',() => {
   onClickAdd();
-})
-//テキストボックスエレメント取得
-//テキストボックス値取得
-
-// テキストボックス初期化
+});
